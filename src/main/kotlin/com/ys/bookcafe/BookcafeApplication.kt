4975acae3c53af13995b5fec4346d353b89dcc80
youngseok <dblys152@gmail.com>
2025-03-14 15:38:33 +0900

    프로젝트 생성

---
+++ src/main/kotlin/com/ys/bookcafe/BookcafeApplication.kt
@@ -0,0 +1,11 @@
+package com.ys.bookcafe
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BookcafeApplication
+
+fun main(args: Array<String>) {
+	runApplication<BookcafeApplication>(*args)
+}
---
+++ src/test/kotlin/com/ys/bookcafe/BookcafeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ys.bookcafe
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BookcafeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
